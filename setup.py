7a5280d062b3dbdaf5debed564ffd6a222e8a981
Sunyeop Lee <sunyeop97@gmail.com>
2019-02-06 19:47:00 +0900

    Add long_description

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 from distutils.core import setup
+from os import path
 
 from setuptools import find_packages
+
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='pscp',
     version='0.0.2',
     description='Per-session checkpoint using git-stash-create',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Sunyeop Lee',
     author_email='sunyeop97@gmail.com',
     url='https://github.com/qbx2/pscp',
